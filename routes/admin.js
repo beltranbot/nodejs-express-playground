7f2f21255195e5461d1becf57f32999a6fc2f5e2
beltranbot <beltranbot@gmail.com>
2021-09-07 01:22:02 +0000

    adjustments

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,15 @@
 const express = require('express')
 
-const productsController = require('../controllers/products')
+const adminController = require('../controllers/admin')
 
 const router = express.Router()
 
 // /admin/add-product => GET
-router.get('/add-product', productsController.getAddProduct)
+router.get('/add-product', adminController.getAddProduct)
+
+router.get('/products', adminController.getProducts)
 
 // /admin/product => POST
-router.post('/product', productsController.postAddProduct)
+router.post('/product', adminController.postAddProduct)
 
 module.exports = router
