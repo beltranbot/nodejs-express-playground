143c2b93506a35a7bc6a99a07635f41e9d3727fb
beltranbot <beltranbot@gmail.com>
2021-09-11 16:47:56 +0000

    added order and order items models

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const Product = require('./models/product')
 const User = require('./models/user')
 const Cart = require('./models/cart')
 const CartItem = require('./models/cart-item')
+const Order = require('./models/order')
+const OrderItem = require('./models/order-item')
 
 const app = express()
 
@@ -53,6 +55,16 @@ Cart.belongsTo(User, {
 })
 Cart.belongsToMany(Product, { through: CartItem })
 Product.belongsToMany(Cart, { through: CartItem })
+Order.belongsTo(User, {
+  foreignKey: { field: 'user_id', allowNull: false },
+  constraints: true,
+  onDelete: 'CASCADE'
+})
+User.hasMany(Order, {
+  foreignKey: 'user_id'
+})
+Order.belongsToMany(Product, { through: OrderItem })
+Product.belongsToMany(Order, { through: OrderItem })
 
 // sequelize.sync({ force: true })
 sequelize.sync() // force: true to force recreation
